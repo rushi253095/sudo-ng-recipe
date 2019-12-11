bc00eef2a0a09d28394f83031aa75078fb12d3f0
Bansari Patel <bansaripatel@SA-MAC-046.local>
2019-12-11 20:13:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecipeComponent
   ],
   imports: [
     BrowserModule,
