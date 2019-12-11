c9e005ba4807dd35465ce55db0641a323d51d3d1
Bansari Patel <bansaripatel@SA-MAC-046.local>
2019-12-11 20:51:51 +0530

    [WIP]:card error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
+import { ViewRecipeComponent } from './components/view-recipe/view-recipe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRecipeComponent
+    AddRecipeComponent,
+    ViewRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[
+    MatCardModule
+  ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/view-recipe/view-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-recipe',
+  templateUrl: './view-recipe.component.html',
+  styleUrls: ['./view-recipe.component.scss']
+})
+export class ViewRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
